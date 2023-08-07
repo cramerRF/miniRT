2cc412184dd48c2044ac5549fc3cfbdb46f23103
CramerRF <victor.alonso.tr@gmail.com>
2023-08-07 14:19:28 +0200

    idk

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
 #include    "../inc/miniRT.h"
 
+t_rt	*open_rt_file(char *name_file)
+{
+	t_rt	*ret;
+
+	ret = malloc(sizeof(t_rt));
+	if (!ret)
+		return (NULL);
+	ft_bzero(ret, sizeof(t_rt));
+	ret->file = ft_strdup(name_file);
+	return (ret);
+}
+
+void	help_use()
+{
+	printf("./miniRT (.rt file)\nOnly one argument\nFile must be format like generated\n\n");
+}
+
+void	set_up_exec(int (*exec[6])(t_rt *rt))
+{
+	exec[EX_ERROR] = NULL;
+	exec[EX_ADD] = NULL;
+	exec[EX_EDIT] = NULL;
+	exec[EX_DELETE] = NULL;
+	exec[EX_EXIT] = NULL;
+	exec[EX_HELP] = NULL;
+}
+
 int main(int argc, char **args)
 {
     t_rt    *rt;
